cbc767f73711a248978cfd350983393aa9ca01f7
Ramfour <bdddanil2002@gmail.com>
2026-03-16 20:13:14 +0700

    init 10_function_object | add femaleName list | add random generate gender | name | surname

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -3,5 +3,7 @@ window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
 };
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -1,6 +1,9 @@
+// Объект с свойствами, которые идут для генерации данных людей
 const personGenerator = {
+    // json'ы нужны для имитации получения данных с сервера файла, формата json
+    // json с фамилиями, count 15 исправлен на 16, иначе "Морозов" никогда не сработает
     surnameJson: `{  
-        "count": 15,
+        "count": 16,
         "list": {
             "id_1": "Иванов",
             "id_2": "Смирнов",
@@ -20,6 +23,7 @@ const personGenerator = {
             "id_16": "Морозов"
         }
     }`,
+    // json с мужскими именами
     firstNameMaleJson: `{
         "count": 10,
         "list": {     
@@ -35,36 +39,63 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    
+    // json с мужскими именами
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Мария",
+            "id_3": "Ирина",
+            "id_4": "Анна",
+            "id_5": "Дарья",
+            "id_6": "Наталья",
+            "id_7": "Татьяна",
+            "id_8": "София",
+            "id_9": "Елена",
+            "id_10": "Ольга"
+        }
+    }`,
 
+    // Глобальные константы полов
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
+    // Метод генерации случайного числа, со сдвигом, где min включительно, а max + 1 не включительно
     randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
 
+    // функция, которая получает json, преобразует в объект
     randomValue: function (json) {
         const obj = JSON.parse(json);
+        // В константу записываем id_ + случайное число из метода randomIntNumber
         const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
+    randomGender: function() {
 
-        return this.randomValue(this.firstNameMaleJson);
+        return (Math.random() < 0.5)? this.GENDER_MALE: this.GENDER_FEMALE;
+    },
+
+    randomFirstName: function(gender) {
+
+        const json = (gender === this.GENDER_MALE)? this.firstNameMaleJson : this.firstNameFemaleJson ;
+        return this.randomValue(json)
 
     },
 
-
-     randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+     randomSurname: function(gender) {
+        const surname = this.randomValue(this.surnameJson);
+        return (gender === this.GENDER_MALE)? surname : surname + 'a' ;
     },
 
 
     getPerson: function () {
+        // Создаем объект пользователя
         this.person = {};
-        // this.person.gender = this.randomGender();
-        this.person.firstName = this.randomFirstName();
+        this.person.gender = this.randomGender();
+        this.person.firstName = this.randomFirstName(this.person.gender);
+        this.person.surname = this.randomSurname(this.person.gender);
         return this.person;
     }
 };
